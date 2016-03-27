e7dec3ef863676261bbbcdaa9229124a8c3a6bb0
cookie <tsubasa-kun@foxmail.com>
2016-03-27 17:46:29 +0800

    first commit

---
+++ app/src/main/java/com/love_cookies/e_tourism/View/Activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.love_cookies.e_tourism.View.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.love_cookies.e_tourism.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
