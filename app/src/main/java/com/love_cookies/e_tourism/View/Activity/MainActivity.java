b28ce5fac29319075d29b41d475cb21cb5b97b2d
Cookie <tsubasa-kun@foxmail.com>
2016-04-11 10:37:25 +0800

    修改主页布局

--- app/src/main/java/com/love_cookies/e_tourism/View/Activity/MainActivity.java
+++ app/src/main/java/com/love_cookies/e_tourism/View/Activity/MainActivity.java
@@ -2,23 +2,17 @@ package com.love_cookies.e_tourism.View.Activity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
 import com.love_cookies.cookie_library.Activity.BaseActivity;
 import com.love_cookies.e_tourism.R;
 
 import org.xutils.view.annotation.ContentView;
-import org.xutils.view.annotation.ViewInject;
 
 @ContentView(R.layout.activity_main)
 public class MainActivity extends BaseActivity {
 
-    @ViewInject(R.id.text_title)
-    TextView titleTV;
-
     @Override
     public void initWidget(Bundle savedInstanceState) {
-        titleTV.setText(R.string.app_name);
     }
 
     @Override
